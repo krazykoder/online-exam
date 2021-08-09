89ae8c47ef236beb53eae9b89e3c9908b4033adc
krazyKoder <sab.sultana@gmail.com>
2021-08-09 13:48:36 -0700

    Customer REST GET only

--- src/com/JPA/onlineExam/controller/CustomerREST.java
+++ src/com/JPA/onlineExam/controller/CustomerREST.java
@@ -1,13 +1,39 @@
 package com.JPA.onlineExam.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.JPA.onlineExam.model.Customer;
+import com.JPA.onlineExam.model.Employee;
+import com.JPA.onlineExam.repository.CustomerRepository;
 
 @RestController
 @RequestMapping(value = "/customers", produces = { MediaType.APPLICATION_JSON_VALUE })
 public class CustomerREST {
 
+	@Autowired
+	CustomerRepository repository;
+
+	public CustomerRepository getRepository() {
+		return repository;
+	}
+
+	public void setRepository(CustomerRepository repository) {
+		this.repository = repository;
+	} 
 	
-	
+	@GetMapping(value = "/ping", produces = { MediaType.TEXT_PLAIN_VALUE })
+	public String allemp() {
+		return "Hello World";
+	}
+
+	@GetMapping(value = "/customers")
+	public List<Customer> getAllEmployees() {
+		return repository.findAll();
+	}
 }
