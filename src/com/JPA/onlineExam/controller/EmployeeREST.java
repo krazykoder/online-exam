b4430dd2509eb06808893c647b2fda9b65aac8aa
krazykoder <sab.sultana@gmail.com>
2021-08-08 20:13:33 -0700

    Employee Rest version 1 : GET, POST done.

--- src/com/JPA/onlineExam/controller/EmployeeREST.java
+++ src/com/JPA/onlineExam/controller/EmployeeREST.java
@@ -1,30 +1,45 @@
 package com.JPA.onlineExam.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.JPA.onlineExam.model.Employee;
 import com.JPA.onlineExam.repository.EmployeeRepository;
 
 @RestController
-@RequestMapping(value = "/employee-management", produces = { MediaType.APPLICATION_JSON_VALUE })
+@RequestMapping(value = "/employees", produces = { MediaType.APPLICATION_JSON_VALUE })
 public class EmployeeREST {
 
 	@Autowired
-	EmployeeRepository employeRepo;
+	EmployeeRepository repository;
 
-	public EmployeeRepository getEmployeRepo() {
-		return employeRepo;
+	public EmployeeRepository getRepository() {
+		return repository;
 	}
 
-	public void setEmployeRepo(EmployeeRepository employeRepo) {
-		this.employeRepo = employeRepo;
+	public void setRepository(EmployeeRepository repository) {
+		this.repository = repository;
 	}
 
 	@GetMapping(value = "/ping", produces = { MediaType.TEXT_PLAIN_VALUE })
 	public String allemp() {
 		return "Hello World";
+	}
+
+	@GetMapping(value = "/employees")
+	public List<Employee> getAllEmployees() {
+		return repository.findAll();
+	}
+
+	@PostMapping("/employees")
+	Employee createOrSaveEmployee(@RequestBody Employee newEmployee) {
+		return repository.save(newEmployee);
 	}
 }
--- src/com/JPA/onlineExam/repoTest/bookTest_applicationContext.java
+++ src/com/JPA/onlineExam/repoTest/bookTest_applicationContext.java
@@ -1,7 +1,5 @@
 package com.JPA.onlineExam.repoTest;
 
-import javax.transaction.Transactional;
-
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -9,7 +7,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import com.JPA.onlineExam.service.BookService;
 
 public class bookTest_applicationContext {
-	@Transactional
+
 	@Test
 	public void bookcall() {
 		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
